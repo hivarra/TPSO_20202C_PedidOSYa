57b999d712aa9ccfea0c076ff11069bd9cd8834b
gonzautn <hgonzalo91@gmail.com>
2020-08-31 15:35:57 -0300

    Fix Shared: executable to shared project

---
+++ shared/shared.c
@@ -0,0 +1,8 @@
+/*
+ * shared.c
+ *
+ *  Created on: 31 ago. 2020
+ *      Author: utnso
+ */
+
+#include "shared.h"
---
+++ shared/shared.h
@@ -0,0 +1,9 @@
+/*
+ * shared.h
+ *
+ *  Created on: 31 ago. 2020
+ *      Author: utnso
+ */
+
+
+#define CONEXIONES_MAXIMAS 10
