3c40cbed236c8bea63165e2d99ab260f327ee678
Jorge <suhjorge@gmail.com>
2020-09-15 15:41:10 -0300

    filesystem

--- sindicato/src/Auxiliares/Filesystem.c
+++ sindicato/src/Auxiliares/Filesystem.c
@@ -14,7 +14,7 @@
 
 void montarFileSystem() {
 
-	if(!existeDirectorio(sindicato_conf.punto_montaje_afip)) {
+//	if(!existeDirectorio(sindicato_conf.punto_montaje_afip)) {
 
 //		crearDirectorio(gamecard_conf.punto_montaje_tallgrass);
 //		crearMetadataGlobal();
@@ -25,7 +25,7 @@ void montarFileSystem() {
 
 		log_info(logger, "File System: Creación finalizada");
 
-	} else {
+//	} else {
 
 //		ruta_files = string_new();
 //		string_append(&ruta_files, gamecard_conf.punto_montaje_tallgrass);
@@ -45,5 +45,39 @@ void montarFileSystem() {
 
 		log_info(logger, "File System: Ya existe");
 	}
+
+//}
+
+void crearDirectorio(char *path) {
+
+	struct stat st = {0};
+
+	if(stat(path, &st) == -1 ) {
+		if(mkdir(path, 0777) == -1) {
+			perror("mkdir");
+			log_error(logger, "CREAR_DIR: ERROR. PERMISOS %s", path);
+		} else {
+			crearMetadataDirectorio(path);
+		}
+	} else {
+//		log_info(logger, "CREAR_DIR: WARNING. YA EXISTE %s", path);
+	}
+
+}
+
+void crearMetadataDirectorio(char* ruta) {
+
+	char* ruta_archivo = string_from_format("%s/Metadata.bin", ruta );
+//	log_info(logger, "%s", ruta_archivo);
+	FILE* fp = fopen(ruta_archivo, "w");
+	char* contenido = string_new();
+	string_append(&contenido, "DIRECTORY=Y");
+
+//	log_info(logger, "Contenido Metadata: %s", contenido);
+
+	fputs(contenido, fp);
+	free(contenido);
+	free(ruta_archivo);
+	fclose(fp);
 
 }
--- sindicato/src/Auxiliares/Filesystem.h
+++ sindicato/src/Auxiliares/Filesystem.h
@@ -8,6 +8,26 @@
 #ifndef AUXILIARES_FILESYSTEM_H_
 #define AUXILIARES_FILESYSTEM_H_
 
+#include "../sindicato.h"
+#include "shared.h"
+
+typedef struct {
+	char directory[200];
+	int32_t size;
+	char blocks[100];
+	char open[10];
+} t_metadata;
+
+int tamanio_bloques;
+int cantidad_bloques;
+char* bmap;
+//t_bitarray *bitmap;
+char* ruta_files;
+char* ruta_bloques;
+
+void montarFileSystem();
+void crearDirectorio(char *path);
+void crearMetadataDirectorio(char* ruta);
 
 
 #endif /* AUXILIARES_FILESYSTEM_H_ */
